c3caa75a41f63a04375fdf0cdd60134a8b99473b
Maik <macgyverx83@gmail.com>
2019-02-03 22:13:51 +0100

    feat(websocket): add simple websocket handler
    this simple websocket client handles the basic communication to the server side socket and handles connection as well as sending and receiving messages

---
+++ src/Kraken.WebSockets/Messages/PingMessage.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Kraken.WebSockets.Messages
+{
+    /// <summary>
+    /// Ping message.
+    /// </summary>
+    public class PingMessage : KrakenMessage
+    {
+        private const string EVENT_TYPE = "ping";
+
+        /// <summary>
+        /// Gets the request identifier.
+        /// </summary>
+        /// <value>The request identifier.</value>
+        [JsonProperty("reqid", Order = 1, NullValueHandling = NullValueHandling.Ignore)]
+        public int? RequestId { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Kraken.WebSockets.Messages.PingMessage"/> class.
+        /// </summary>
+        /// <param name="requestId">Request identifier.</param>
+        public PingMessage(int? requestId = null) : base(EVENT_TYPE)
+        {
+            RequestId = requestId;
+        }
+    }
+}
