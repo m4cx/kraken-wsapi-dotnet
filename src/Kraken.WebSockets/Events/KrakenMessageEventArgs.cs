1129896f5bf6504696380856d1537d2b152a58dc
Maik <macgyverx83@gmail.com>
2019-02-08 23:00:31 +0100

    refactor: do not pass serializer instance around
    the serializer is only used and accessed in needed places - not in events passed through the whole system

--- src/Kraken.WebSockets/Events/KrakenMessageEventArgs.cs
+++ src/Kraken.WebSockets/Events/KrakenMessageEventArgs.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Kraken.WebSockets.Messages;
 
 namespace Kraken.WebSockets.Events
 {
@@ -8,37 +7,29 @@ namespace Kraken.WebSockets.Events
     /// </summary>
     public class KrakenMessageEventArgs : EventArgs
     {
-        private readonly IKrakenMessageSerializer serializer;
+        /// <summary>
+        /// Gets the event.
+        /// </summary>
+        /// <value>The event.</value>
+        /// <remarks>Can be null or empty string</remarks>
+        public string Event { get; }
 
         /// <summary>
         /// Gets the raw content of the message.
         /// </summary>
         /// <value>The content of the raw.</value>
+        /// <remarks>Can be null or empty string</remarks>
         public string RawContent { get; }
-
-        /// <summary>
-        /// Gets the message.
-        /// </summary>
-        /// <returns>The message.</returns>
-        /// <typeparam name="TKrakenMessage">The 1st type parameter.</typeparam>
-        public TKrakenMessage GetMessage<TKrakenMessage>() where TKrakenMessage : IKrakenMessage
-        {
-            return serializer.Deserialize<TKrakenMessage>(RawContent);
-        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:Kraken.WebSockets.Events.KrakenMessageEventArgs"/> class.
         /// </summary>
+        /// <param name="event">Event identifier</param>
         /// <param name="rawContent">Raw content.</param>
-        public KrakenMessageEventArgs(string rawContent, IKrakenMessageSerializer serializer)
+        public KrakenMessageEventArgs(string @event, string rawContent)
         {
-            RawContent = rawContent ?? throw new ArgumentNullException(nameof(rawContent));
-            this.serializer = serializer;
-
-            if (string.Empty.Equals(rawContent))
-            {
-                throw new ArgumentOutOfRangeException(nameof(rawContent));
-            }
+            Event = @event;
+            RawContent = rawContent;
         }
     }
 }
