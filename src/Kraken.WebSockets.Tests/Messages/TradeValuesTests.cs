d351adc5e5dc85c14c240500e1de931eca704bf0
Maik <macgyverx83@gmail.com>
2019-04-06 16:45:38 +0200

    feat(trade): handle trade messages from websocket

---
+++ src/Kraken.WebSockets.Tests/Messages/TradeValuesTests.cs
@@ -0,0 +1,30 @@
+﻿using Kraken.WebSockets.Messages;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Kraken.WebSockets.Tests.Messages
+{
+    [ExcludeFromCodeCoverage]
+    public class TradeValuesTests
+    {
+        #region CreateFromJArray()
+
+        [Fact]
+        public void CreateFromJArray_Test()
+        {
+            var tradeValuesJArray = JArray.Parse(@"[""5541.20000"",""0.15850568"",""1534614057.321597"",""s"",""l"",""""]");
+            var tradeValues = TradeValues.CreateFromJArray(tradeValuesJArray);
+
+            Assert.NotNull(tradeValues);
+            Assert.Equal(5541.20000M, tradeValues.Price);
+            Assert.Equal(0.15850568M, tradeValues.Volume);
+            Assert.Equal(1534614057.321597M, tradeValues.Time);
+            Assert.Equal("s", tradeValues.Side);
+            Assert.Equal("l", tradeValues.OrderType);
+            Assert.Equal(string.Empty, tradeValues.Misc);
+        }
+
+        #endregion
+    }
+}
