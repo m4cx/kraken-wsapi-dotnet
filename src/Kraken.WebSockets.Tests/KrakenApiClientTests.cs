29577b722f8a4d6e3459753cd60ac6474056147b
Maik <macgyverx83@gmail.com>
2019-04-09 21:31:38 +0200

    chore: add tests for data message handling

--- src/Kraken.WebSockets.Tests/KrakenApiClientTests.cs
+++ src/Kraken.WebSockets.Tests/KrakenApiClientTests.cs
@@ -45,7 +45,7 @@ namespace Kraken.WebSockets.Tests
         #region SubscribeAsync()
 
         [Fact]
-        public async Task SubscribeAsync_SubscribeNull_ThrowsArgumentNullException() 
+        public async Task SubscribeAsync_SubscribeNull_ThrowsArgumentNullException()
             => Assert.Equal("subscribe", (await Assert.ThrowsAsync<ArgumentNullException>(() => instance.SubscribeAsync(null))).ParamName);
 
         [Fact]
@@ -53,7 +53,7 @@ namespace Kraken.WebSockets.Tests
         {
             var subscribe = new Subscribe(new string[] { "XBT/EUR" }, new SubscribeOptions(SubscribeOptionNames.All));
             await instance.SubscribeAsync(subscribe);
-            socket.Verify( mock => mock.SendAsync(It.Is<Subscribe>(x => x == subscribe)), Times.Once);
+            socket.Verify(mock => mock.SendAsync(It.Is<Subscribe>(x => x == subscribe)), Times.Once);
         }
 
         #endregion
@@ -97,7 +97,7 @@ namespace Kraken.WebSockets.Tests
                 .Returns(TestSocketMessages.SubscriptionStatus1);
 
             socket.Raise(x => x.DataReceived += null, new KrakenMessageEventArgs(SubscriptionStatus.EventName, TestSocketMessages.SubscriptionStatus1Message));
-            
+
             Assert.Equal(1, instance.Subscriptions.Count);
             Assert.Equal(TestSocketMessages.SubscriptionStatus1, instance.Subscriptions[TestSocketMessages.SubscriptionStatus1.ChannelId.Value]);
         }
@@ -120,7 +120,7 @@ namespace Kraken.WebSockets.Tests
         #region UnsubscribeAsync()
 
         [Fact]
-        public async Task UnsubscribeAsync_ChannelIdZero_ThrowsArgumentOutOfRangeException() 
+        public async Task UnsubscribeAsync_ChannelIdZero_ThrowsArgumentOutOfRangeException()
             => Assert.Equal("channelId", (await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => instance.UnsubscribeAsync(0))).ParamName);
 
         [Fact]
@@ -128,6 +128,158 @@ namespace Kraken.WebSockets.Tests
         {
             await instance.UnsubscribeAsync(123);
             socket.Verify(x => x.SendAsync(It.Is<Unsubscribe>(y => y.ChannelId == 123)));
+        }
+
+        #endregion
+
+        #region KrakenDataMessageHandling
+
+        [Fact]
+        public void KrakenDataMessage_TickerIsReceivedAndPropagatedThoughEvent()
+        {
+            instance.Subscriptions.Add(0, new SubscriptionStatus() { ChannelId = 0, Pair = "EUR/XBT", Subscription = new SubscribeOptions(SubscribeOptionNames.Ticker) });
+            bool handlerExecuted = false;
+            instance.TickerReceived += (sender, e) =>
+            {
+                Assert.Equal(0, e.ChannelId);
+                Assert.Equal("EUR/XBT", e.Pair);
+                Assert.IsType<TickerMessage>(e.DataMessage);
+                handlerExecuted = true;
+            };
+
+            socket.Raise(x => x.DataReceived += null, new KrakenMessageEventArgs("data", TestSocketMessages.TickerMessage, 0));
+            Assert.True(handlerExecuted);
+        }
+
+        [Fact]
+        public void KrakenDataMessage_OhlcIsReceivedAndPropagatedThroughEvent()
+        {
+            const int expectedChannelId = 42;
+            instance.Subscriptions.Add(expectedChannelId, new SubscriptionStatus() { ChannelId = expectedChannelId, Pair = "EUR/XBT", Subscription = new SubscribeOptions(SubscribeOptionNames.OHLC) });
+            bool handlerExecuted = false;
+            instance.OhlcReceived += (sender, e) =>
+            {
+                Assert.Equal(expectedChannelId, e.ChannelId);
+                Assert.Equal("EUR/XBT", e.Pair);
+                Assert.IsType<OhlcMessage>(e.DataMessage);
+                handlerExecuted = true;
+            };
+
+            socket.Raise(x => x.DataReceived += null, new KrakenMessageEventArgs("data", TestSocketMessages.OhlcMessage, expectedChannelId));
+            Assert.True(handlerExecuted);
+        }
+
+        [Fact]
+        public void KrakenDataMessage_TradeIsReceivedAndPropagatedThroughEvent()
+        {
+            const int expectedChannelId = 0;
+            instance.Subscriptions.Add(expectedChannelId, new SubscriptionStatus() { ChannelId = expectedChannelId, Pair = "EUR/XBT", Subscription = new SubscribeOptions(SubscribeOptionNames.Trade) });
+            bool handlerExecuted = false;
+            instance.TradeReceived += (sender, e) =>
+            {
+                Assert.Equal(expectedChannelId, e.ChannelId);
+                Assert.Equal("EUR/XBT", e.Pair);
+                Assert.IsType<TradeMessage>(e.DataMessage);
+                handlerExecuted = true;
+            };
+
+            socket.Raise(x => x.DataReceived += null, new KrakenMessageEventArgs("data", TestSocketMessages.TradeMessage, expectedChannelId));
+            Assert.True(handlerExecuted);
+        }
+
+        [Fact]
+        public void KrakenDataMessage_SpreadIsReceivedAndPropagatedThroughEvent()
+        {
+            const int expectedChannelId = 0;
+            instance.Subscriptions.Add(
+                expectedChannelId,
+                new SubscriptionStatus()
+                {
+                    ChannelId = expectedChannelId,
+                    Pair = "EUR/XBT",
+                    Subscription = new SubscribeOptions(SubscribeOptionNames.Spread)
+                });
+            bool handlerExecuted = false;
+            instance.SpreadReceived += (sender, e) =>
+            {
+                Assert.Equal(expectedChannelId, e.ChannelId);
+                Assert.Equal("EUR/XBT", e.Pair);
+                Assert.IsType<SpreadMessage>(e.DataMessage);
+                handlerExecuted = true;
+            };
+
+            socket.Raise(x => x.DataReceived += null,
+                new KrakenMessageEventArgs("data", TestSocketMessages.SpreadMessage, expectedChannelId));
+            Assert.True(handlerExecuted);
+        }
+
+        [Fact]
+        public void KrakenDataMessage_BookSnapshotIsReceivedAndPropagatedThroughEvent()
+        {
+            const int expectedChannelId = 0;
+            instance.Subscriptions.Add(
+                expectedChannelId,
+                new SubscriptionStatus()
+                {
+                    ChannelId = expectedChannelId,
+                    Pair = "EUR/XBT",
+                    Subscription = new SubscribeOptions(SubscribeOptionNames.Book)
+                });
+            bool handlerExecuted = false;
+            instance.BookSnapshotReceived += (sender, e) =>
+            {
+                Assert.Equal(expectedChannelId, e.ChannelId);
+                Assert.Equal("EUR/XBT", e.Pair);
+                Assert.IsType<BookSnapshotMessage>(e.DataMessage);
+                handlerExecuted = true;
+            };
+
+            socket.Raise(x => x.DataReceived += null,
+                new KrakenMessageEventArgs("data", TestSocketMessages.BookSnapshotMessage, expectedChannelId));
+            Assert.True(handlerExecuted);
+        }
+
+        [Fact]
+        public void KrakenDataMessage_BookUpdateWithCompleteDataIsReceivedAndPropagatedThroughEvent()
+        {
+            const int expectedChannelId = 1234;
+            instance.Subscriptions.Add(
+                expectedChannelId,
+                new SubscriptionStatus()
+                {
+                    ChannelId = expectedChannelId,
+                    Pair = "EUR/XBT",
+                    Subscription = new SubscribeOptions(SubscribeOptionNames.Book)
+                });
+            bool handlerExecuted = false;
+            instance.BookUpdateReceived += (sender, e) =>
+            {
+                Assert.Equal(expectedChannelId, e.ChannelId);
+                Assert.Equal("EUR/XBT", e.Pair);
+                Assert.IsType<BookUpdateMessage>(e.DataMessage);
+                handlerExecuted = true;
+            };
+
+            socket.Raise(x => x.DataReceived += null,
+                new KrakenMessageEventArgs("data", TestSocketMessages.BookUpdateCompleteMessage, expectedChannelId));
+            Assert.True(handlerExecuted);
+        }
+
+        [Fact]
+        public void KrakenDataMessage_UnknownDataNoEventIsEmitted()
+        {
+            bool handlerExecuted = false;
+            Action handler = () => handlerExecuted = true;
+            instance.BookSnapshotReceived += (sender, e) => handler();
+            instance.BookUpdateReceived += (sender, e) => handler();
+            instance.TradeReceived += (sender, e) => handler();
+            instance.SpreadReceived += (sender, e) => handler();
+            instance.OhlcReceived += (sender, e) => handler();
+            instance.TradeReceived += (sender, e) => handler();
+
+            socket.Raise(x => x.DataReceived += null,
+                new KrakenMessageEventArgs("data", TestSocketMessages.BookUpdateCompleteMessage, 1));
+            Assert.False(handlerExecuted);
         }
 
         #endregion
