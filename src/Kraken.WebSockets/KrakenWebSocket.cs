517a77d3f9e840b2c80643eef8ba80cf177425f8
m4cx <macgyverx83@gmail.com>
2020-04-11 17:51:52 +0200

    feat(kraken-web-socket): pass cancellation-token to underlying socket

--- src/Kraken.WebSockets/KrakenWebSocket.cs
+++ src/Kraken.WebSockets/KrakenWebSocket.cs
@@ -36,9 +36,18 @@ namespace Kraken.WebSockets
         public event EventHandler<KrakenMessageEventArgs> DataReceived;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:Kraken.WebSockets.KrakenWebsocket"/> class.
+        /// Initializes a new instance of the <see cref="T:Kraken.WebSockets.KrakenWebsocket" /> class.
         /// </summary>
         /// <param name="uri">URI.</param>
+        /// <param name="serializer">The serializer.</param>
+        /// <param name="webSocket">The web socket.</param>
+        /// <exception cref="ArgumentNullException">
+        /// uri
+        /// or
+        /// serializer
+        /// or
+        /// webSocket
+        /// </exception>
         public KrakenWebSocket(string uri, IKrakenMessageSerializer serializer, IWebSocket webSocket)
         {
             this.uri = uri ?? throw new ArgumentNullException(nameof(uri));
@@ -49,14 +58,14 @@ namespace Kraken.WebSockets
         /// <summary>
         /// Connect to the websocket server.
         /// </summary>
-        /// <returns>The connect.</returns>
-        public async Task ConnectAsync()
+        /// <param name="cancellationToken"></param>
+        public async Task ConnectAsync(CancellationToken cancellationToken = default)
         {
             try
             {
-                logger.LogInformation("Trying to connect to '{uri}'", uri);
+                logger?.LogInformation("Trying to connect to '{uri}'", uri);
 
-                await webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
+                await webSocket.ConnectAsync(new Uri(uri), cancellationToken);
                 InvokeConnected();
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 StartListening();
@@ -64,7 +73,7 @@ namespace Kraken.WebSockets
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, "Error while connecting to '{uri}'", uri);
+                logger?.LogError(ex, "Error while connecting to '{uri}'", uri);
                 throw;
             }
         }
@@ -74,24 +83,22 @@ namespace Kraken.WebSockets
         /// </summary>
         /// <returns>The async.</returns>
         /// <param name="message">Message.</param>
-        public async Task SendAsync<TKrakenMessage>(TKrakenMessage message) where TKrakenMessage : IKrakenMessage
+        public async Task SendAsync<TKrakenMessage>(TKrakenMessage message, CancellationToken cancellationToken = default)
+            where TKrakenMessage : IKrakenMessage
         {
             if (webSocket.State == WebSocketState.Open)
             {
-                var jsonMessage = serializer.Serialize<TKrakenMessage>(message);
-                logger.LogTrace("Trying to send: {message}", jsonMessage);
+                var jsonMessage = serializer.Serialize(message);
+
+                logger?.LogTrace("Trying to send: {@message}", jsonMessage);
 
                 var messageBytes = DEFAULT_ENCODING.GetBytes(jsonMessage);
-                await webSocket.SendAsync(
-                    new ArraySegment<byte>(messageBytes),
-                    WebSocketMessageType.Text,
-                    true,
-                    CancellationToken.None);
-                logger.LogTrace("Successfully sent: {message}", jsonMessage);
+                await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, cancellationToken);
+                logger?.LogTrace("Successfully sent: {@message}", jsonMessage);
                 return;
             }
 
-            logger.LogWarning("WebSocket is not open. Current state: {state}",
+            logger?.LogWarning("WebSocket is not open. Current state: {state}",
                 webSocket.State);
         }
 
@@ -99,30 +106,27 @@ namespace Kraken.WebSockets
         /// Closes the websocket.
         /// </summary>
         /// <returns>The async.</returns>
-        public async Task CloseAsync()
+        public async Task CloseAsync(CancellationToken cancellationToken = default)
         {
             if (webSocket.State == WebSocketState.Open)
             {
-                await webSocket.CloseAsync(
-                    WebSocketCloseStatus.NormalClosure,
-                    "Connection closed by consumer",
-                    CancellationToken.None);
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Connection closed by consumer", cancellationToken);
             }
         }
 
         #region Private Helper
 
-        private async Task StartListening()
+        private async Task StartListening(CancellationToken cancellationToken = default)
         {
             try
             {
                 while (webSocket.State == WebSocketState.Open)
                 {
-                    logger.LogDebug("Waiting for new message");
-                    var message = await ReadNextMessage();
+                    logger?.LogDebug("Waiting for new message");
+                    var message = await ReadNextMessage(cancellationToken);
 
-                    logger.LogDebug("Received new message from websocket");
-                    logger.LogTrace("Received: {message}", message);
+                    logger?.LogDebug("Received new message from websocket");
+                    logger?.LogTrace("Received: '{message}'", message);
 
                     string eventString = null;
                     int? channelId = null;
@@ -147,25 +151,25 @@ namespace Kraken.WebSockets
                                 eventString = channelId != null ? "data" : "private";
                                 break;
                         }
-                    }
 
-                    InvokeDataReceived(new KrakenMessageEventArgs(eventString, message, channelId));
+                        InvokeDataReceived(new KrakenMessageEventArgs(eventString, message, channelId));
+                    }
                 }
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, "Error while listening or reading new messages from WebSocket");
+                logger?.LogError(ex, "Error while listening or reading new messages from WebSocket");
                 // TODO: Disconnected-Event
                 throw;
             }
             finally
             {
-                logger.LogInformation("Closing WebSocket");
+                logger?.LogInformation("Closing WebSocket");
                 webSocket.Dispose();
             }
         }
 
-        private async Task<string> ReadNextMessage()
+        private async Task<string> ReadNextMessage(CancellationToken cancellationToken = default)
         {
             var buffer = new byte[1024];
             var messageParts = new StringBuilder();
@@ -173,13 +177,12 @@ namespace Kraken.WebSockets
             WebSocketReceiveResult result;
             do
             {
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
                 if (result.MessageType == WebSocketMessageType.Close)
                 {
-                    logger.LogDebug("Closing connection to socket");
-                    await
-                        webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
-                    logger.LogDebug("Connection successfully closed");
+                    logger?.LogDebug("Closing connection to socket");
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, cancellationToken);
+                    logger?.LogDebug("Connection successfully closed");
                     // TODO: Disconnected-Event
                 }
                 else
@@ -187,6 +190,7 @@ namespace Kraken.WebSockets
                     var str = Encoding.UTF8.GetString(buffer, 0, result.Count);
                     messageParts.Append(str);
                 }
+
             } while (!result.EndOfMessage);
 
             var message = messageParts.ToString();
